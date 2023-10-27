f4e490c265c6bcc8917389b1e59c10c49e951135
colloso999 <tripperskripper@gmail.com>
2023-10-27 09:01:53 +0300

    simple_pagination

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -10,6 +10,8 @@ from typing import List
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     '''
+    Finds the correct indexes to paginate the dataset correctly and return the
+    appropriate page of the dataset.
     The function should return a tuple of size two containing a start index and
     an end index corresponding to the range of indexes to return in a list for
     those particular pagination parameters.
