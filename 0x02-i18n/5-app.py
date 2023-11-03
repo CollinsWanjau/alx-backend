8eaddb2d45009bd2c34d9585f71f7fe5f1ee7031
colloso999 <tripperskripper@gmail.com>
2023-11-03 10:41:16 +0300

    Mock logging in

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -45,10 +45,13 @@ def get_user():
 
 @app.before_request
 def before_request():
+    """
+    Used to reigster a function to run before each request.
+    """
     g.user = get_user()
 
 
-@babel.localeselector
+# @babel.localeselector
 def get_locale():
     """Determine the best match with supported languages
 
@@ -61,7 +64,7 @@ def get_locale():
         return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-# babel.init_app(app, locale_selector=get_locale)
+babel.init_app(app, locale_selector=get_locale)
 
 
 @app.route('/')
